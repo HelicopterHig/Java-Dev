07a3bec348d08f34a7e5e34e7848659fa11bfc29
HelicopterHig <32367339+HelicopterHig@users.noreply.github.com>
2018-10-07 17:33:51 +0300

    Update OOP_LAB_2.java

--- OOP_LAB_2.java
+++ OOP_LAB_2.java
@@ -1,10 +1,13 @@
 package com.company;
 import java.lang.String;
-public class Main {
+public class OOP_LAB_2
+{
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("Amount element of args: " + args.length);
-        for (int i = 0; i < args.length; i++) {
+        for (int i = 0; i < args.length; i++)
+        {
             System.out.println(args[i] + "  ");
         }
     }
